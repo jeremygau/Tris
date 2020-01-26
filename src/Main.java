5165d1967eaba54963bf4a44d58f9f8ab7a32d69
jeremy.gau <caliban3010@gmail.com>
2020-01-26 16:22:25 +0100

    Decompte du temps

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,38 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int size = 30;
-        List<Integer> suite = new ArrayList<>(size);
+        int size = 3126;
+        List<Integer> suite0 = new ArrayList<>(size);
+        List<Integer> suite1 = new ArrayList<>(size);
+        List<Integer> suite2 = new ArrayList<>(size);
+        List<Integer> suite3 = new ArrayList<>(size);
+
         Random rand = new Random();
         for (int i = 0; i < size; i++) {
-            suite.add(i, rand.nextInt(100));
+            int r = rand.nextInt(100000);
+            suite0.add(i, r);
+            suite1.add(i, r);
+            suite2.add(i, r);
+            suite3.add(i, r);
         }
 
-        System.out.println(suite);
+        System.out.println(suite0);
 
-        List<Sort<Integer>> sorts = new ArrayList<>();
-        sorts.add(new BubbleSort<>());
-        sorts.add(new FusionSort<>());
-        sorts.add(new QuickSort<>());
-        sorts.add(new PileSort<>());
+        Map<Sort<Integer>, List<Integer>> sorts = new HashMap<>();
+        sorts.put(new BubbleSort<>(), suite0);
+        sorts.put(new FusionSort<>(), suite1);
+        sorts.put(new QuickSort<>(), suite2);
+        sorts.put(new PileSort<>(), suite3);
 
-        for (Sort<Integer> sort : sorts) {
+        for (Sort<Integer> sort : sorts.keySet()){
             System.out.println(sort.getClass().getName());
-            System.out.println(sort.sort(suite));
+            System.out.println("suite non triée \t" + sorts.get(sort));
+            long startTime = System.currentTimeMillis();
+            System.out.println("suite triée \t \t" + sort.sort(sorts.get(sort)));
+            System.out.println("temps d'execution = " + (System.currentTimeMillis() - startTime) + " ms\n");
         }
     }
 }
