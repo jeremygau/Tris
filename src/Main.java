21c33d97217a7fa7c3f3687c862a6ee82d826633
jeremy.gau <caliban3010@gmail.com>
2020-02-14 09:42:39 +0100

    r

--- src/Main.java
+++ src/Main.java
@@ -15,20 +15,20 @@ public class Main {
 //        list.add(1);
 //        list.add(1);
 
-        int[] list = {0,2,0,1,2,1,0,2,1,1};
-//        CountingSort<Integer> countingSort = new CountingSort<>(list);
-//        System.out.println(countingSort.counting(list));
-        DenombSort denombSort = new DenombSort(list, 3);
-        System.out.println(disp(denombSort.sort()));
+//        int[] list = {0,2,0,1,2,1,0,2,1,1};
+////        CountingSort<Integer> countingSort = new CountingSort<>(list);
+////        System.out.println(countingSort.counting(list));
+//        DenombSort denombSort = new DenombSort(list, 3);
+//        System.out.println(disp(denombSort.sort()));
 
 //        int[] a = denombSort.counting(list);
 //        System.out.println(disp(a));
 //        int[] b = denombSort.position(a);
 //        System.out.println(disp(b));
 
-//        int size = 10;
-//        Solver<Integer> solver = new Solver<>(size, "int");
-//        solver.solve();
+        int size = 1000;
+        Solver<String> solver = new Solver<>(size, "string");
+        solver.solve();
     }
     public static String disp(int[] t) {
         StringBuilder s = new StringBuilder("[");
